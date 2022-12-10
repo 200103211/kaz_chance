607cb9965faa94d22d5e95eaec78a1782d1f7adc
Gulzhan <200103211@stu.sdu.edu.kz>
2022-12-11 05:09:55 +0600

    1.1.1

--- app/src/main/java/com/example/kaz_chance/activities/LogRegActivity.kt
+++ app/src/main/java/com/example/kaz_chance/activities/LogRegActivity.kt
@@ -1,9 +1,10 @@
-package com.example.kaz_chance
+package com.example.kaz_chance.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kaz_chance.R
 
-class MainActivity : AppCompatActivity() {
+class LogRegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/kaz_chance/activities/TopActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kaz_chance.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kaz_chance.R
+
+class TopActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top)
+    }
+}
